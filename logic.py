dc9feb8205b8c6ba750b6274f35cde7a4605f814
czett <keinaniemand@gmail.com>
2024-11-30 14:56:17 +0100

    basic switch mechanics, including destroying it

--- logic.py
+++ logic.py
@@ -28,6 +28,9 @@ class State:
         
         q.append(self)
 
+    def __str__(self) -> str:
+        return f"q{self.num}"
+
 class Automaton:
     def __init__(self, atype:str, states:list) -> None:
         self.atype = atype
@@ -37,16 +40,17 @@ class Automaton:
         self.states.append(state)
 
 start = State(True, False, 0)
-end = State(False, True, 1)
+broken = State(False, False, 1)
+end = State(False, True, 2)
 
-connect(start, end, "D", "AN")
-connect(end, start, "D", "AUS")
+connect(start, end, "D", "ON") # turn switch on
+connect(end, start, "D", "OFF") # turn switch off
+connect(end, broken, "S", "BROKEN") # break switch, maybe destroying it for context, only possible when turned on
 
 def run(word:str):
     spos = []
     for index, t in enumerate(transitions):
         if t["s1"] == start:
-            print("yay")
             spos.append(t)
 
     if spos == []:
@@ -54,13 +58,24 @@ def run(word:str):
     
     current_state = start
 
-    for symbol in word:
+    for index, symbol in enumerate(word):
+        state_found = False
+        symbol_found = False
+
         for t in transitions:
-            if t["s1"] == current_state and t["input"] == symbol:
-                print(t["output"])
-                current_state = t["s2"]
-                break
+            if t["s1"] == current_state:
+                state_found = True
+                if t["input"] == symbol:
+                    print(t["output"])
+                    current_state = t["s2"]
+                    symbol_found = True
+                    break
+
+        if not state_found:
+            return f"no transition from state: {current_state} found :("
+        elif not symbol_found:
+            return f"no transition with symbol: {symbol} at index {index} found :("
 
     return "success, done"
 
-print(run("DDD"))
+print(run("DDDSD"))
